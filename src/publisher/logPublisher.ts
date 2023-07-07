80498518dde52d23c7d4cd514e05a2733d27ddc0
petretiandrea <petretiandrea@gmail.com>
2023-07-07 14:23:55 +0200

    added json formatter

--- src/publisher/logPublisher.ts
+++ src/publisher/logPublisher.ts
@@ -1,16 +1,21 @@
 import { Logger } from "@nestjs/common";
 import { OutboxMessage } from "../source/sourceConnector";
 import { Publisher } from "./publisher";
+import { DocumentFormatter, simplifiedFormatter } from "./jsonFormatter";
 
 
 export class LogPublisher implements Publisher {
 
     constructor(
-        private readonly logger: Logger = new Logger("FakePublisher")
+        private readonly logger: Logger = new Logger("LogPublisher"),
+        private readonly formatter: DocumentFormatter = simplifiedFormatter
     ) { }
 
     send(message: OutboxMessage): Promise<OutboxMessage> {
-        this.logger.log("Fake publish event: " + JSON.stringify(message));
+        const payload = this.formatter(message.value);
+        this.logger.log(
+            `Event to publish with key ${message.key}\npayload ${payload}`,
+        )
         return Promise.resolve(message);
     }
 }
