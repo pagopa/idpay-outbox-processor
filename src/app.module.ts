99442cc5f9ca68fa74a3b5e161053c59da276258
petretiandrea <petretiandrea@gmail.com>
2023-07-07 16:54:27 +0200

    added right health check for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
+import { Inject, Logger, Module, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
 import { appConfig } from './config';
 import { PipelineConfig, PipelineFactory } from './pipeline/pipelineFactory';
 import { TerminusModule } from '@nestjs/terminus';
@@ -7,6 +7,8 @@ import { MongoSourceCdcConnectorFactory } from './source/mongo-cdc/mongoSourceCd
 import { KafkaPublisher } from './publisher/kafkaPublisher';
 import { LogPublisher } from './publisher/logPublisher';
 import { PipelineJob } from './pipeline/pipelineJob';
+import { MongoSourceCdcConnector } from './source/mongo-cdc/mongoSourceCdcConnector';
+import { Publisher } from './publisher/publisher';
 
 @Module({
     imports: [
@@ -14,34 +16,48 @@ import { PipelineJob } from './pipeline/pipelineJob';
     ],
     controllers: [
         HealthController
+    ],
+    providers: [
+        {
+            provide: MongoSourceCdcConnector,
+            useFactory: async () => {
+                return await MongoSourceCdcConnectorFactory.fromConfig({
+                    uri: appConfig.mongoDbUri,
+                    dbName: appConfig.mongoDbName,
+                    collectionName: appConfig.outboxCollectionName,
+                    aggregationPipeline: [
+                        { $match: { operationType: { $in: ["insert", "update", "replace"] } } },
+                        { $project: { _id: 1, fullDocument: 1, ns: 1, documentKey: 1 } },
+                    ],
+                });
+            },
+        },
+        {
+            provide: 'Publisher',
+            useFactory: () => {
+                return appConfig.kafkaConfig ? KafkaPublisher.fromConfig({
+                    broker: appConfig.kafkaConfig.broker,
+                    topic: appConfig.kafkaConfig.topic,
+                    sasl_jaas: appConfig.kafkaConfig.saslJaas
+                }) : new LogPublisher()
+            }
+        }
     ]
 })
 export class AppModule implements OnApplicationBootstrap, OnApplicationShutdown {
 
     private pipeline?: PipelineJob
 
-    constructor() { }
+    constructor(
+        @Inject(MongoSourceCdcConnector) private readonly source: MongoSourceCdcConnector,
+        @Inject('Publisher') private readonly publisher: Publisher
+    ) {
+    }
 
     async onApplicationBootstrap() {
-        const source = await MongoSourceCdcConnectorFactory.fromConfig({
-            uri: appConfig.mongoDbUri,
-            dbName: appConfig.mongoDbName,
-            collectionName: appConfig.outboxCollectionName,
-            aggregationPipeline: [
-                { $match: { operationType: { $in: ["insert", "update", "replace"] } } },
-                { $project: { _id: 1, fullDocument: 1, ns: 1, documentKey: 1 } },
-            ],
-        });
-
-        const publisher = appConfig.kafkaConfig ? KafkaPublisher.fromConfig({
-            broker: appConfig.kafkaConfig.broker,
-            topic: appConfig.kafkaConfig.topic,
-            sasl_jaas: appConfig.kafkaConfig.saslJaas
-        }) : new LogPublisher()
-
         // pipeline job
         const pipelineConfig: PipelineConfig = { name: "defaultPipeline", type: "full" }
-        PipelineFactory.fromConfig(pipelineConfig, source, publisher)
+        PipelineFactory.fromConfig(pipelineConfig, this.source, this.publisher)
             .then(job => {
                 this.pipeline = job;
                 job.start();
